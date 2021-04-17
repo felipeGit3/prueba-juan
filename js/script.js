fc5cefe380a7558a72bd52a27b1ba6107c15863c
felipeGit3 <fepevi@gmail.com>
2021-04-16 23:48:21 -0300

    primera subida a GitHub

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function cargarVideo(obj,tipoVideo)
+{
+    //alert("hola");
+    obj.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/USDX0X-d588" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    /*srcDelYoutube = obj.dataset.embed;
+    if (tipoVideo == 1)
+        obj.innerHTML = '<div class="youtube"><iframe frameborder="0" allowfullscreen="" src="https://www.youtube.com/embed/' + srcDelYoutube + '?rel=0&amp;showinfo=0&amp;autoplay=1" allow="autoplay"></iframe></div>';
+    else if (tipoVideo == 2)
+        obj.innerHTML = '<div class="caja-1"><iframe class="imagen-contenedor-video" frameborder="0" allowfullscreen="" src="https://www.youtube.com/embed/' + srcDelYoutube + '?rel=0&amp;showinfo=0&amp;autoplay=1" allow="autoplay"></iframe></div>';
+    else if (tipoVideo == 3)
+        obj.innerHTML = '<div class="youtube"><div class="contenedor-youtube"><iframe frameborder="0" allowfullscreen="" src="https://www.youtube.com/embed/' + srcDelYoutube + '?rel=0&amp;showinfo=0&amp;autoplay=1" allow="autoplay"></iframe></div></div>';*/
+}
